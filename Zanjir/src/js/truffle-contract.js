7086514abed7ec88a29c396164feb1f883dd3e7e
maqzi <munaf.qazi@gmail.com>
2021-07-03 21:31:07 -0400

    scripts removed

--- Zanjir/src/js/truffle-contract.js
+++ Zanjir/src/js/truffle-contract.js
@@ -2,7 +2,7 @@
 (function (global){
 var ethJSABI = require("ethjs-abi");
 var BlockchainUtils = require("truffle-blockchain-utils");
-var Web3 = require("web3");
+var Web3 = require('web3');
 
 // For browserified version. If browserify gave us an empty version,
 // look for the one provided by the user.
@@ -25,7 +25,7 @@ var contract = (function(module) {
     return this.provider.sendAsync.apply(this.provider, arguments);
   };
 
-  var BigNumber = (new Web3()).toBigNumber(0).constructor;
+  var BigNumber = (new Web3(Web3.givenProvider || "ws://localhost:8545")).toBigNumber(0).constructor;
 
   var Utils = {
     is_object: function(val) {
